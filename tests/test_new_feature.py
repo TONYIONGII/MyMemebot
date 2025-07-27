c058bf6adcd031225d0fbc0f7e42b231ea322f6d
Io Hong Iong <iohongiong@IodeMacBook-Pro.local>
2025-07-27 18:27:39 +0800

    完善新功能：添加输入类型检查

--- tests/test_new_feature.py
+++ tests/test_new_feature.py
@@ -2,5 +2,8 @@ import unittest
 from meme_tracker.new_feature import new_feature
 
 class TestNewFeature(unittest.TestCase):
-    def test_feature(self):
-        self.assertIsNone(new_feature())
+    def test_success_case(self):
+        self.assertEqual(new_feature(5), 10)
+    
+    def test_error_case(self):
+        self.assertIsNone(new_feature("invalid"))
